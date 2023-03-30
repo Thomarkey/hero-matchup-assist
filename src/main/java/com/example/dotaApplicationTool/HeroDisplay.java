21049f195b9c4e4e912fe54f72c74b28b23474ca
Thomas Markey <thoma_cz@hotmail.com>
2023-03-30 14:57:21 +0200

    added hero display

---
+++ src/main/java/com/example/dotaApplicationTool/HeroDisplay.java
@@ -0,0 +1,58 @@
+package com.example.dota2demotool;
+
+import com.mashape.unirest.http.*;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Iterator;
+
+
+@RestController
+public class HeroDisplay {
+
+    final String URL = "https://api.stratz.com/api/v1/Hero";
+    final String BEARER = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJuYW1laWQiOiJodHRwczovL3N0ZWFtY29tbXVuaXR5LmNvbS9vcGVuaWQvaWQvNzY1NjExOTc5NzgzMzE0OTgiLCJ1bmlxdWVfbmFtZSI6IlRPTUFUT1JPIiwiU3ViamVjdCI6ImYzNDg2ODVjLWYyYTAtNDllZS1hNGYwLWM5ZmUwYjc4OWUyYiIsIlN0ZWFtSWQiOiIxODA2NTc3MCIsIm5iZiI6MTY4MDE2NTg3MCwiZXhwIjoxNzExNzAxODcwLCJpYXQiOjE2ODAxNjU4NzAsImlzcyI6Imh0dHBzOi8vYXBpLnN0cmF0ei5jb20ifQ.3o_pX3YaUn-oVp4x_LeBfkkp9xoSRIaFolcYwnx5mJc";
+
+    public String getAllHeroes() throws UnirestException {
+        Unirest.setTimeouts(0, 0);
+        HttpResponse<String> response = Unirest.get(URL)
+                .header("Authorization", "Bearer " + BEARER)
+                .asString();
+        return response.getBody();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/heroes")
+    public String getHeroes() throws UnirestException {
+        return getAllHeroes();
+    }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/heroes/{name}")
+    public String getHeroByName(@PathVariable String name) throws UnirestException, JSONException {
+        return returnHero(name);
+    }
+
+    public String returnHero(String name) throws UnirestException, JSONException {
+
+        String jsonString = getAllHeroes();
+        JSONObject obj = new JSONObject(jsonString);
+        String heroJson = null;
+
+        Iterator<String> keys = obj.sortedKeys();
+        while (keys.hasNext()) {
+            String key = keys.next();
+            JSONObject hero = obj.getJSONObject(key);
+            String displayName = hero.getString("displayName");
+            if (displayName.equals(name)) {
+                heroJson = hero.toString();
+            }
+        }
+
+        return heroJson;
+    }
+
+}
