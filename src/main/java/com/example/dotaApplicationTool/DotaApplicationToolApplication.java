78f3c89e0ca218924cb43591ce7cb7c451231874
Thomas Markey <thoma_cz@hotmail.com>
2023-03-30 14:46:04 +0200

    Initial commit

---
+++ src/main/java/com/example/dotaApplicationTool/DotaApplicationToolApplication.java
@@ -0,0 +1,13 @@
+package com.example.dotaApplicationTool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DotaApplicationToolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DotaApplicationToolApplication.class, args);
+	}
+
+}
