06e8a22cc3eface7d797aade7dbdd7bbf5e9fa9a
thomarkey <thoma_cz@hotmail.com>
2023-06-19 13:43:30 +0200

    updates angular to deploy to heroku

--- Frontend/server.js
+++ Frontend/server.js
@@ -1,16 +1,10 @@
-//Install express server
 const express = require('express');
-const path = require('path');
-
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/Frontend'));
+app.use(express.static('./dist/'));
 
-app.get('/*', function (req, res) {
-
-    res.sendFile(path.join(__dirname + '/dist/Frontend/index.html'));
+app.get('/*', (req, res) => {
+    res.sendFile('index.html', { root: 'dist/' });
 });
 
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
