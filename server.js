a115cccc861110fa50b1e815f8606cb784463299
thomarkey <thoma_cz@hotmail.com>
2023-06-19 16:10:59 +0200

    updated server.js to point to correct dist folder

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use(express.static('./dist/'));
+app.use(express.static('Frontend/dist'));
 
 app.get('/*', (req, res) => {
-    res.sendFile('index.html', { root: 'dist/' });
+  res.sendFile('index.html', { root: 'Frontend/dist' });
 });
 
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, () => {
+  console.log('Server is running on port', process.env.PORT || 8080);
+});
