c98dd581bd8a4fa47506773e6de0da6b914dd5b6
thomarkey <thoma_cz@hotmail.com>
2023-06-19 16:50:46 +0200

    updated env api url, updated --prod build flag

--- Frontend/src/environments/environment.prod.ts
+++ Frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    backendApiUrl: 'https://hero-matchup-assistant.herokuapp.com/',
+    backendApiUrl: 'https://hero-matchup-assistant-25269114e6bf.herokuapp.com/',
 };
