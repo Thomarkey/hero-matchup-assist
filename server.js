295a6803fca186f4e39787246c8456450de2ae4a
thomarkey <thoma_cz@hotmail.com>
2023-06-20 23:29:06 +0200

    updated header for CORS

--- server.js
+++ server.js
@@ -21,6 +21,7 @@ app.use(express.static(distPath, { setHeaders: (res, filePath) => {
 // Define the middleware function
 const setJsonContentType = (req, res, next) => {
   res.setHeader('Content-Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Origin', 'https://dota-hero-matchup-cfe3a3ef874f.herokuapp.com');
   next(); // Call next to proceed to the next middleware or route handler
 };
 
