742062999486408438cb2f93ac681b824c2643d8
thomarkey <thoma_cz@hotmail.com>
2023-06-22 18:51:40 +0200

    added host to server.js

--- server.js
+++ server.js
@@ -29,6 +29,8 @@ const setJsonContentType = (req, res, next) => {
 app.use('/', setJsonContentType);
 
 const port = process.env.PORT || 8080;
-app.listen(port, () => {
+const host = process.env.HOST ||'0.0.0.0';
+
+app.listen(port, host, () => {
   console.log('Server is running on port', port);
 });
